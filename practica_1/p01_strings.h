d633c6cfc8a30be28566d233127dedc5a96b1d43
Eric <alu0101335339@ull.edu.es>
2022-09-29 12:39:35 +0100

    practica01 inicializada

---
+++ practica_1/p01_strings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Cadena {
+ public:
+  Cadena(void) : simbolo_(NULL) {} 
+ private:
+  char* simbolo_;
+};
---
+++ practica_1/p01_strings_main.cc
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Universidad de La Laguna
+ * Escuela Superior de Ingenieria y Tecnologia
+ * Grado en Ingenieria Informatica
+ * Curso: 2do
+ * @category Computabilidad y Algoritmia
+ * @file p01_strings_main.cc
+ * @author Eric Angueta Rogel || alu0101335339@ull.edu.es
+ * @brief p01_strings_main.cc: programa cliente.
+ *        Contiene la funcion main del proyecto que usa las clases p01_strings.cc y p01_strings.h
+ *        para hacer operaciones con las cadenas dadas a partir de un fichero.
+ * @version 0.1
+ * @date 2022-09-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <fstream>
+
+#include "p01_strings.h"
+
+int main(int argc, char* argv[]) {
+  
+}
