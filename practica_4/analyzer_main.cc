2ad979c9701bde37909f0ccf3c88fd20d74b2098
Eric1408 <alu0101335339@ull.edu.es>
2022-10-25 23:23:36 +0100

    subido

--- practica_4/analyzer_main.cc
+++ practica_4/analyzer_main.cc
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
+
+#include "analyzer.h"
 
 int main(int argc, char* argv[]) {
   std::string name{argv[1]};
-  std::string out{argv[2]};
-
   std::ifstream code{name};
-  std::ofstream output{out};
-  std::string aux;
+  std::string lines;
+  std::string saved_code;
+  std::regex expression("int");
+  std::smatch matches;
+  
+  while (getline(code, lines)) {
+    saved_code += lines + "\n";
 
-  while (getline(code, aux)) {
-    std::cout << aux << std::endl;
   }
+  std::regex_search(saved_code , matches, expression);
+
+  //std::cout << saved_code;
+  
+  for (auto match: matches) {
+    std::cout << match << std::endl;
+  }
+  std::cout << "mathces: ";
+  std::cout << matches.length() << std::endl;
 
   return 0;
 }
---
+++ practica_4/usage.h
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Universidad de La Laguna
+ * Escuela Superior de Ingenieria y Tecnologia
+ * Grado en Ingenieria Informatica
+ * Curso: 2do
+ * @category Computabilidad y Algoritmia
+ * @file usage.h
+ * @author Eric Angueta Rogel || alu0101335339@ull.edu.es
+ * @version 0.1
+ * @date 2022-09-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+
+
+const std::string kExplain  {"El modo de uso de este programa es el siguiente:\n"
+                          "./CalculadoraRPN <entrada.txt> "};
+const std::string kHelp {"./lenguajesP02: faltan o se ha pasado del numero de argumentos.\n"
+                          "Pruebe './CalculadoraRPN --help' para mas informacion\n"};
+void Usage(int, char*[]);
