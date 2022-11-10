b4d22e6fc073a76874fadb92ac6cf644c0e194f4
Eric1408 <alu0101335339@ull.edu.es>
2022-11-10 14:49:40 +0000

    subido en guagua

--- practica_6/automata_main.cc
+++ practica_6/automata_main.cc
@@ -9,8 +9,16 @@ int main(int argc, char* argv[]) {
   Usage(argc, argv);
   std::ifstream file_in{argv[1]};
   std::ifstream file_strings{argv[2]};
+  std::string lines;
 
   Automata autom(file_in);
+  while (getline(file_strings, lines)) {
+    if (autom.CheckString(lines)) {
+      std::cout << lines << " --- Accepted\n";
+    } else {
+      std::cout << lines << " --- Rejected\n";
+    }
+  }
   //for (int i = 0; i < autom.GetTotalStates(); ++i) {
   //  std::cout << "Desde " << i << " transiciona ";
   //  std::cout << autom.GetEstados(i);
