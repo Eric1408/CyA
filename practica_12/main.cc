af6c9101e45fa7e178f1ae65e978eb8ebdf42489
Eric1408 <alu0101335339@ull.edu.es>
2023-01-10 10:37:05 +0000

    nada

---
+++ practica_12/main.cc
@@ -0,0 +1,32 @@
+/**
+ * 
+ * Universidad de La Laguna
+ * Escuela Superior de Ingenieria y Tecnologia
+ * Grado en Ingenieria Informatica
+ * Curso: 2do
+ * @category Computabilidad y Algoritmia
+ * @file automata_main.cc
+ * @brief Fcihero que main que contiene las clase automata para generar uno 
+ * @author Eric Angueta Rogel || alu0101335339@ull.edu.es
+ * @version 0.1
+ * @date 2022-09-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//#include "usage.h"
+//#include "karatsuba.h"
+
+int main(int argc, char* argv[]) {
+  std::cout << "bien";
+  int a = atoi(argv[1]);
+  std::cout << argc << std::endl;
+  std::cout << argv[1] << std::endl;
+  
+  return 0;
+}
